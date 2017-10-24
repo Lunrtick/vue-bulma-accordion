2d48ccb5a0de6efbc7dc2f9f1b62537deb2cbb06
Gabriel Soicher <gabe@mainyak.com>
2017-10-24 13:38:24 +0200

    renovations - use cli template for publishing

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue-bulma-accordion
-"A simple accordion or collapsible styled with Bulma, animated by Velocity and sprinkled with Font Awesome.
--- plugin.js
+++
@@ -1,9 +0,0 @@
-import BulmaAccordion from './BulmaAccordion'
-import BulmaAccordionItem from './BulmaAccordionItem'
-
-module.exports = {
-    install: function (Vue, options) {
-        Vue.component('bulma-accordion', BulmaAccordion);
-        Vue.component('bulma-accordion-item', BulmaAccordionItem);
-    }
-};
